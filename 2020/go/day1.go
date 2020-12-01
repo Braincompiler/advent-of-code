877987f22bee2eb611191ab71473cd7c3a38a15a
Christian Seifert <seifert@braincompiler.de>
2020-12-01 23:11:54 +0100

    Day 1

---
+++ 2020/go/day1.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func day1() {
+	file, err := os.Open("../inputs/day1.txt")
+	if err != nil {
+		panic("Failed to open input file 1 for day 1")
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	var numbersPuzzle1 []int
+	var numbersPuzzle2 []int
+	for scanner.Scan() {
+		s := scanner.Text()
+		n, err := strconv.Atoi(s)
+		if err != nil {
+			panic(fmt.Sprintf("Failed to convert %s to an integer number", s))
+		}
+
+		numbersPuzzle1 = append(numbersPuzzle1, n)
+		if len(s) == 3 {
+			numbersPuzzle2 = append(numbersPuzzle2, n)
+		}
+	}
+
+	file.Close()
+
+	resPuzzle1 := findResultPuzzle1(numbersPuzzle1)
+	fmt.Println(fmt.Sprintf("Answer for Puzzle 1 at Day 1: %d", resPuzzle1))
+
+	resPuzzle2 := findResultPuzzle2(numbersPuzzle2)
+	fmt.Println(fmt.Sprintf("Answer for Puzzle 2 at Day 1: %d", resPuzzle2))
+}
+
+func findResultPuzzle1(numbers []int) int {
+	size := len(numbers)
+	for i := 0; i < size; i++ {
+		prev := numbers[i]
+		for j := i + 1; j < size; j++ {
+			if prev+numbers[j] == 2020 {
+				return prev * numbers[j]
+			}
+		}
+	}
+
+	return -1
+}
+
+func findResultPuzzle2(numbers []int) int {
+	size := len(numbers)
+	for i := 0; i < size; i++ {
+		prev := numbers[i]
+		for j := i + 1; j < size; j++ {
+			prev2 := numbers[j]
+			for k := j + 1; k < size; k++ {
+				if prev+prev2+numbers[k] == 2020 {
+					return prev * prev2 * numbers[k]
+				}
+			}
+		}
+	}
+
+	return -1
+}
